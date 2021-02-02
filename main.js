c83735cb05b4b961a25c73a2f78c59b71efab99b
Vadim <vadim.mozeiko@gmail.com>
2021-02-02 14:59:48 +0200

    Todo

--- main.js
+++ main.js
@@ -1,66 +1,38 @@
 import {Todo} from './components/Todo.js'
-import {Car} from './components/Car.js'
-
-
-// const myCar = new Car('Volvo', 30, 5, 0, 30)
-
-// myCar.carDetails()
-
-// myCar.engineON()
-
-// myCar.drive(100)
-// myCar.drive(100)
-// myCar.drive(100)
-// myCar.drive(100)
-// myCar.drive(100)
-// myCar.drive(50)
-// myCar.drive(200)
-
-// myCar.carDetails()
-
-// myCar.fillup(10)
-// myCar.fillup(7)
-
-// myCar.carDetails()
-
-// myCar.fillup(15)
-
-// myCar.carDetails()
 
 
 
 
-
-// const doToday = new Todo('Today\'s list')
-
-
-// doToday.add('Clean my room')
-// doToday.add('Do workout')
-// doToday.add('Study')
-// doToday.add('Charge my phone')
-// doToday.add('Check the weather')
+const doToday = new Todo('Today\'s list')
 
 
+doToday.add('Clean my room')
+doToday.add('Do workout')
+doToday.add('Study')
+doToday.add('Charge my phone')
+doToday.add('Check the weather')
 
-// doToday.list()
-
-// doToday.edit(0, 'Wash my clothes')
-// doToday.remove(0)
 
 
-// doToday.print()
+doToday.list()
 
-// doToday.list()
+doToday.edit(0, 'Wash my clothes')
+doToday.remove(0)
 
-// doToday.changeStatus(1, true)
-// doToday.changeStatus(2, true)
 
-// doToday.print()
+doToday.print()
 
-// doToday.remove(10)
+doToday.list()
 
-// doToday.print()
+doToday.changeStatus(1, true)
+doToday.changeStatus(2, true)
 
-// doToday.selectCompletedOnly()
+doToday.print()
 
-// doToday.selectNotCompleted()
+doToday.remove(10)
+
+doToday.print()
+
+doToday.selectCompletedOnly()
+
+doToday.selectNotCompleted()
